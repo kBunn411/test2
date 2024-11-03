6ca69157e9cd375fd9ee5c4d8d5e91b7c406c878
kBunn411 <117461180+kBunn411@users.noreply.github.com>
2024-11-03 13:51:57 -0500

    Should be done

--- middleware.ts
+++ middleware.ts
@@ -3,5 +3,5 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware();
 
 export const config = {
-  matcher: ["/", "/forget-password", "/api/webhooks/clerk"], // Specify the routes where this middleware should apply
+  matcher: ["/", "/forget-password", "/api/webhooks/chef"], // Specify the routes where this middleware should apply
 };
