75cc7ee4166524207188a47cd49384aa669fb282
HarpinderSingh1999 <tui57086@temple.edu>
2024-11-06 12:55:25 -0500

    Baseline for how it is going to look. We could change this however you guys want whenever, BUT there is not a lot of stuff for mobile/tablet looking good.

--- app/random/page.tsx
+++ app/random/page.tsx
@@ -1,27 +1,38 @@
-"use client"
+"use client";
 import { RecipeResult } from "@/types/RecipeResponseType";
 import { searchRecipes } from "@/utils/fetchRecipes";
 import { useEffect, useState } from "react";
-import styles from "@/app/styles.module.css"
+import styles from "@/app/styles.module.css";
 import RecipeCard from "@/components/RecipeCard";
 
-const RandomPage = () =>{ 
-    const [recipes, setRecipes] = useState<RecipeResult[]>()
+const RandomPage = () => {
+    const [recipes, setRecipes] = useState<RecipeResult[]>([]);
 
-	useEffect(() => {
-		const effect = async () => {
-			const recipeResult = await searchRecipes();
-      if(recipeResult){
-        setRecipes(recipeResult.map((result:any) =>{return{title:result.recipe.label, image: result.recipe.image, link:result.recipe.shareAs}}))
-      }
-		};
-		effect();
-	}, []);
-    return (<div className={styles.container}><div className={styles.recipes}>
-    {recipes ? recipes.map((recipe, key) =>
-        <RecipeCard key={key} recipe={recipe}/>
-    ):<h1>Loading Random recipes</h1>}
-    </div></div>)
-}
+    useEffect(() => {
+        const effect = async () => {
+            const recipeResult = await searchRecipes(); // No arguments for random search
+            if (recipeResult) {
+                setRecipes(recipeResult.map((result: any) => ({
+                    title: result.recipe.label,
+                    image: result.recipe.image,
+                    link: result.recipe.shareAs
+                })));
+            }
+        };
+        effect();
+    }, []);
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.recipes}>
+                {recipes.length > 0 ? (
+                    recipes.map((recipe, key) => <RecipeCard key={key} recipe={recipe} />)
+                ) : (
+                    <h1>Loading Random Recipes</h1>
+                )}
+            </div>
+        </div>
+    );
+};
 
 export default RandomPage;
