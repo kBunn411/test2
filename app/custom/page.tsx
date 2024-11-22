11005a5d6ed92c1e45be23202bc4720975721f0b
mihirp11 <114366288+mihirp11@users.noreply.github.com>
2024-11-22 01:44:13 -0500

    styliing for openai page

--- app/custom/page.tsx
+++ app/custom/page.tsx
@@ -1,13 +1,12 @@
 "use client";
-import styles from "@/app/styles.module.css";
+import Button from "@/components/Button/Button";
+import styles from "./openai.module.css";
 import { useState } from "react";
 
 export default function Generate() {
     const [theInput, setTheInput] = useState("");
     const [isLoading, setIsLoading] = useState(false);
-    const [responseContent, setResponseContent] = useState(
-        "Enter the ingredients of the recipe you would like to generate."
-    );
+    const [responseContent, setResponseContent] = useState([""]);
 
     const prompt = "Give me a recipe with the ingredients below: ";
 
@@ -28,11 +27,13 @@ export default function Generate() {
             const output = await response.json();
 
             if (output) {
-                setResponseContent(output.content); // Update with the assistant's latest response
+                const splitOutput = output.content.split("\n");
+                console.log(splitOutput);
+                setResponseContent(splitOutput); // Update with the assistant's latest response
             }
         } catch (error) {
             console.error("Error fetching response:", error);
-            setResponseContent("An error occurred. Please try again.");
+            setResponseContent([]);
         } finally {
             setIsLoading(false);
         }
@@ -46,32 +47,40 @@ export default function Generate() {
     };
 
     return (
-        <div className={styles.card}>
-            <div
-                style={{
-                    width: "max",
-                    maxWidth: "18rem",
-                    padding: "3px 4px",
-                    height: "min",
-                }}
-                className="w-max max-w-[18rem] rounded-md px-4 py-3 h-min bg-gray-200 text-gray-800"
-            >
-                {responseContent}
+        <div className={styles.container}>
+            <div>
+                {
+                    "Enter the ingredients of the recipe you would like to generate, or ask our A.I. to make a special dish!"
+                }
             </div>
             <textarea
+                style={{ width: "80%", height: "150px" }}
                 value={theInput}
                 onChange={e => setTheInput(e.target.value)}
-                className="w-[85%] h-10 px-3 py-2 resize-none overflow-y-auto text-black bg-gray-300 rounded-l outline-none"
                 onKeyDown={handleSubmit}
                 disabled={isLoading}
             />
-            <button
-                onClick={callGetResponse}
-                className="w-[15%] bg-blue-500 px-4 py-2 rounded-r"
-                disabled={isLoading}
-            >
-                {isLoading ? "Loading..." : "Generate"}
-            </button>
+            <Button
+                style={{ width: "35%" }}
+                onClick={isLoading ? callGetResponse : () => {}}
+                text={isLoading ? "Loading..." : "Generate"}
+            />
+            {responseContent && responseContent.length && (
+                <div
+                    style={{
+                        marginTop: "2rem",
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "0.5rem",
+                        alignItems: "flex-start",
+                        paddingLeft: "1rem",
+                    }}
+                >
+                    {responseContent?.map((recipeLine: string, index) => {
+                        return <span key={index}>{recipeLine}</span>;
+                    })}
+                </div>
+            )}
         </div>
     );
 }
--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,7 +1,20 @@
+import { CSSProperties } from "react";
 import styles from "./button.module.css";
-const Button = ({ text, onClick }: { text: string; onClick: () => any }) => {
+const Button = ({
+    text,
+    onClick,
+    style,
+}: {
+    text: string;
+    onClick: () => any;
+    style?: CSSProperties;
+}) => {
     return (
-        <button className={styles.button} onClick={onClick}>
+        <button
+            className={styles.button}
+            onClick={onClick}
+            style={{ ...style }}
+        >
             {text}
         </button>
     );
